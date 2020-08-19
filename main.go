4e0152c7e288ffefece07c5674a2089e63e9cd62
Yash Ladha <yashladhapankajladha123@gmail.com>
2020-08-19 15:17:56 +0530

    fix: deadlock in the logic
    Fetching the stale branches in unbuffered channel and hence the buffer
    size is not defined it is not blocked on the receiver to pop from the
    channel. Hence somethings racefull condition is happening because data
    is not present in the correct fashion. Using a buffered channel makes it
    much more faster and simpler to handle the concurrency.

--- main.go
+++ main.go
@@ -12,13 +12,14 @@ func filterOlderBranches(branchArr []string, oldChan chan []string, wg *sync.Wai
 
 	app := "git"
 	ret := []string{}
+
 	for _, branchName := range branchArr {
 		if len(branchName) == 0 {
 			continue
 		}
 
 		strippedBranchName := branchName[1 : len(branchName)-1]
-		args := []string{"log", "-1", "--since=\"1 year ago\"", strippedBranchName}
+		args := []string{"log", "-1", "--since=\"1 months ago\"", strippedBranchName}
 		cmd := exec.Command(app, args...)
 		stdOut, err := cmd.Output()
 
@@ -64,42 +65,38 @@ func removeBranches(branches []string, removeChan chan int, wg *sync.WaitGroup) 
 }
 
 func fetchOldBranches(outArr []string, wg *sync.WaitGroup) (olderBranches []string) {
-	oldChan := make(chan []string)
+	oldChan := make(chan []string, 20)
 
 	for i := 0; i < len(outArr); i += 10 {
 		wg.Add(1)
 		go filterOlderBranches(outArr[i:min(i+10, len(outArr))], oldChan, wg)
 	}
 
-	go func() {
-		for res := range oldChan {
-			olderBranches = append(olderBranches, res...)
-		}
-	}()
-
 	wg.Wait()
 	close(oldChan)
+
+	for res := range oldChan {
+		olderBranches = append(olderBranches, res...)
+	}
 
 	return
 }
 
 func removeStaleBranches(olderBranches []string, wg *sync.WaitGroup) int {
-	removeChan := make(chan int)
+	removeChan := make(chan int, 20)
 
 	for i := 0; i < len(olderBranches); i += 10 {
 		wg.Add(1)
 		go removeBranches(olderBranches[i:min(i+10, len(olderBranches))], removeChan, wg)
 	}
 
-	staleBranches := 0
-	go func() {
-		for cnt := range removeChan {
-			staleBranches += cnt
-		}
-	}()
-
 	wg.Wait()
 	close(removeChan)
+
+	staleBranches := 0
+	for cnt := range removeChan {
+		staleBranches += cnt
+	}
 
 	return staleBranches
 }
